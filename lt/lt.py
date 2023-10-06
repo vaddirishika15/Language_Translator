41134661a4864473b1553f1edcdf900d83ce9b0c
VADDI RISHIKA <120818025+vaddirishika15@users.noreply.github.com>
2023-10-06 23:14:26 +0530

    i

--- lt/lt.py
+++ lt/lt.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from googletrans import Translator
-translator=Translator()
+from translate import Translator
+#translator=Translator()
 # Streamlit app
 def main():
     st.title("Language Translator")
@@ -11,7 +11,12 @@ def main():
         translation=translate(input_text,source_lang,target_lang)
         st.write(f"Translated Text ({target_lang}):",translation)
 def translate(input_text,source_lang,target_lang):
-    translation=translator.translate(input_text,src=source_lang,dest=target_lang)
-    return translation.text
+    try:
+        translator = Translator(to_lang=target_lang, from_lang=source_lang)
+        translation=translator.translate(input_text)
+        return translation
+    except Exception as e:
+        return f"Translation Error: {str(e)}"
+
 if __name__ == "__main__":
     main()
