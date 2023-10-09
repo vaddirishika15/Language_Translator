2a01932a2771eb7dc6ae438d5f0f0660e22cd662
VADDI RISHIKA <120818025+vaddirishika15@users.noreply.github.com>
2023-10-09 20:04:28 +0530

    i

--- lt/lt.py
+++ lt/lt.py
@@ -1,19 +1,20 @@
 import streamlit as st
 from translate import Translator
-#translator=Translator()
+
 # Streamlit app
 def main():
     st.title("Language Translator")
-    input_text=st.text_area("Enter a sentence:","Text here...")
-    source_lang=st.selectbox("Select Source Language:",["auto", "en", "de"])
-    target_lang=st.selectbox("Select Target Language:",["en", "de"])
+    input_text = st.text_area("Enter a sentence:", "Text here...")
+    source_lang = st.selectbox("Select Source Language:", ["auto", "en", "de"])
+    target_lang = st.selectbox("Select Target Language:", ["en", "de"])
     if st.button("Translate"):
-        translation=translate(input_text,source_lang,target_lang)
-        st.write(f"Translated Text ({target_lang}):",translation)
-def translate(input_text,source_lang,target_lang):
+        translation = translate(input_text, source_lang, target_lang)
+        st.write(f"Translated Text ({target_lang}):", translation)
+
+def translate(input_text, source_lang, target_lang):
     try:
         translator = Translator(to_lang=target_lang, from_lang=source_lang)
-        translation=translator.translate(input_text)
+        translation = translator.translate(input_text)
         return translation
     except Exception as e:
         return f"Translation Error: {str(e)}"
